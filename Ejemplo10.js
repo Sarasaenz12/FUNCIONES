0555738ab1701c179a70436c632a4b562b3ce47f
Sarasaenz12 <svsanchez_223@cue.edu.co>
2024-04-15 22:01:14 -0500

    first commit

---
+++ Ejemplo10.js
@@ -0,0 +1,23 @@
+function sumaDivisores(num) {
+    if (!Number.isInteger(num)) {
+        return 'El número debe ser entero';
+    }
+
+    num = Math.abs(num);
+
+    let suma = 0;
+
+    for (let i = 1; i < num; i++) {
+        if (num % i === 0) {
+            suma += i;
+        }
+    }
+
+    return suma;
+}
+
+
+console.log(sumaDivisores(12));
+console.log(sumaDivisores(-6));
+console.log(sumaDivisores(1));
+console.log(sumaDivisores(3.5));
